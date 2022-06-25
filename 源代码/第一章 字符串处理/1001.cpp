77ad019e44027a4cf02a8f2f78a738b94d26840e
XiangMang <91712505+XiangMang@users.noreply.github.com>
2022-06-25 16:14:16 +0800

    对1001源代码添加注释

--- 源代码/第一章 字符串处理/1001.cpp
+++ 源代码/第一章 字符串处理/1001.cpp
@@ -12,11 +12,11 @@ int main(void){
     string num = to_string(a + b);
 
     string ans;
-
-    for(int i = num.size() - 1, j = 0; i >= 0; i--){
-        ans = num[i] + ans;
+    for(int i = str.size() - 1, j = 0; i >= 0; i--){
+        ans = str[i] + ans;
         j++;
-        if(i && j % 3 == 0 && num[i - 1] != '-') ans = ',' + ans;
+        if(j % 3 == 0 && i && str[i - 1] != '-') ans = ',' + ans;
+        //每三位数加上逗号，且避免首次运算与符号
     }
 
     cout << ans << endl;
